d217261d32129a5dbbf6f6f72676c5fe2d6455f9
Iván Lorenzo <elhibar77@hotmail.es>
2025-09-25 14:21:19 +0200

    CM-60 feat: Route created for Patient creattion page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom';
 import Home from "../pages/HomePage";
+import PatientCreationPage from "../pages/admin/PatientCreation/PatientCreationPage";
 
 
 export default function AppRoutes () {
     return (
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/create-patients" element={<PatientCreationPage />} />
+          
           </Routes>  
     )
   
