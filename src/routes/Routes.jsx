7262628f465c7e12e514e539e2fdf4868806a135
Ivan Lorenzo <elhibar77@hotmail.es>
2025-09-25 19:40:00 +0200

    CM-60 feat: Register page route implemented

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,12 @@ import {Routes, Route} from 'react-router-dom';
 import Home from "../pages/HomePage";
 import PatientCreationPage from "../pages/admin/PatientCreation/PatientCreationPage";
 
-
+import RegistrationPage from "../pages/registration/RegistrationPage";
 export default function AppRoutes () {
     return (
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegistrationPage />} />
           <Route path="/create-patients" element={<PatientCreationPage />} />
           
           </Routes>  
