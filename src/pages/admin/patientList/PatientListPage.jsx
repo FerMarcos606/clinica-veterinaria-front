518d3d12b417051b63b12a5874421cf800a4b07b
Yolanda <yolandaalfonsolopez@gmail.com>
2025-09-25 14:50:16 +0200

    CM-25 feat: Hero created

---
+++ src/pages/admin/patientList/PatientListPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../../../components/hero/Hero";
+import Button from "../../../components/button/Button";
+
+export const PatientList = () => {
+    return (
+        <>
+        <Hero text="Listado de pacientes"></Hero>
+        </>
+    )
+}
+
+export default PatientList
