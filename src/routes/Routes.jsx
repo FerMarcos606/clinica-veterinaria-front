8e4423cb4eefb3dabe1afbe85421f76ecefde577
Ivan Lorenzo <elhibar77@hotmail.es>
2025-09-26 11:31:12 +0200

    CM-60 feat: customer area route created

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Routes, Route} from 'react-router-dom';
 import Home from "../pages/HomePage";
 import PatientCreationPage from "../pages/admin/PatientCreation/PatientCreationPage";
+import CustomerArea from "../pages/customer_area/CustomerArea";
 
 import RegistrationPage from "../pages/registration/RegistrationPage";
 export default function AppRoutes () {
@@ -10,8 +11,10 @@ export default function AppRoutes () {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegistrationPage />} />
           <Route path="/create-patients" element={<PatientCreationPage />} />
+          <Route path="/customer-area" element={<CustomerArea />} />
+          </Routes>
           
-          </Routes>  
+
     )
   
 }
