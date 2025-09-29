3564ed015fe455cbecb86c76422c14e615109647
Ivan Lorenzo <elhibar77@hotmail.es>
2025-09-29 09:27:07 +0200

    CM-29 feat: appointments service and repository created

---
+++ src/services/appointments/AppointmentsService.jsx
@@ -0,0 +1,34 @@
+import AppointmentsRepository from '../../repositories/appointments/AppointmentsRepository';
+
+class AppointmentsService {
+    constructor() {
+        this.appointmentsRepository = new AppointmentsRepository();
+    }
+
+    async createAppointment(appointmentData) {
+        try {
+            const result = await this.appointmentsRepository.create(appointmentData);
+
+            console.log('Cita creada con éxito:', result);
+            return result;
+        } catch (error) {
+            console.error('Error en AppointmentsService.createAppointment:', error);
+            throw error;
+        }
+    }
+
+    async getAppointments() {
+        try {
+            const result = await this.appointmentsRepository.getAll();
+            console.log('Citas obtenidas con éxito:', result);
+            return result;
+        } catch (error) {
+            console.error('Error en AppointmentsService.getAppointments:', error);
+            throw error;
+        }
+    }
+}
+
+const appointmentsService = new AppointmentsService();
+
+export default appointmentsService;
